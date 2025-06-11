3fdc0daaa65b2f4dcdcf58036c4198ed32aac4ee
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-11 17:06:00 -0600

    security added to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,24 @@
-const DashboardPage = () => (
-  <div>
-    Home page
-  </div>
-);
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import DashboardView from "@dentist/module/dashboard/ui/view/dashboard-view";
+import { auth } from "@dentist/utils/auth";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
+
+  return (
+    <div>
+      {JSON.stringify(session, null, 2)}
+      <DashboardView />
+    </div>
+  );
+};
  
 export default DashboardPage;
