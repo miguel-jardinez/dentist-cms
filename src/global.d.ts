119705f1f21b033bbd185d7efd7eb14bb4d46ac4
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-10 21:07:02 -0600

    feat: env variables added with types

---
+++ src/global.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_URL: string
+    BASE_URL_APP: string
+    BETTER_AUTH_SECRET: string
+    BETTER_AUTH_URL: string
+    GOOGLE_CLIENT_ID: string
+    GOOGLE_CLIENT_SECRET: string
+    RESEND_DENTIST_CMS: string
+  }
+}
