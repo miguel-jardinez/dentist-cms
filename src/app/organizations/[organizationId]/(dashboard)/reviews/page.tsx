a319d05969f216bf272c78e4ab197ad728de2ff1
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-13 17:25:22 -0600

    update: guard for review page

--- src/app/organizations/[organizationId]/(dashboard)/reviews/page.tsx
+++ src/app/organizations/[organizationId]/(dashboard)/reviews/page.tsx
@@ -1,7 +1,19 @@
-const ReviewsPage = () => (
-  <div>
-    <h1>Reviews</h1>
-  </div>
-);
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import ReviewsView from "@dentist/module/reviews/ui/views/reviews-view";
+import { auth } from "@dentist/utils/auth";
+
+const ReviewsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
+
+  return <ReviewsView />;
+};
  
 export default ReviewsPage;
---
+++ src/module/reviews/ui/views/reviews-view.tsx
@@ -0,0 +1,7 @@
+const ReviewsView = () => (
+  <div>
+    <h1>Reviews</h1>
+  </div>
+);
+ 
+export default ReviewsView;
