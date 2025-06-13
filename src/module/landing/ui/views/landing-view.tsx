c55cb95c77f442f1b29e9548f96d5f9c93d8c04c
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-13 13:50:30 -0600

    feat: landing page view

--- src/module/landing/ui/views/landing-view.tsx
+++ src/module/landing/ui/views/landing-view.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import Link from "next/link";
-
-import { Button } from "@dentist/components/ui/button";
-
 const LandingView = () => (
   <>
     <div className="mx-auto container">
-      <Button asChild>
-        <Link href="/auth/sign-in">Sign in</Link>
-      </Button>
+      Marketing page
     </div>
   </>
 );
