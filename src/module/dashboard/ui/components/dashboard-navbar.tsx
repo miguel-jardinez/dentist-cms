e5ae5b9308c8e100a316ee0b013ccfd6c27a5582
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-13 17:26:20 -0600

    fix: typo

--- src/module/dashboard/ui/components/dashboard-navbar.tsx
+++ src/module/dashboard/ui/components/dashboard-navbar.tsx
@@ -22,7 +22,7 @@ const DashboardNavbar = () => {
         Search
 
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ">
-          <span className="text-sx">&#8984;</span>K
+          <span className="text-xs">&#8984;</span>K
         </kbd>
       </Button>
     </nav>
