c53e4605eaef25c5f25fc9278f65727913112423
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-20 18:13:36 -0600

    update: collaborator view

--- src/module/colaborators/ui/views/colaborators-view.tsx
+++ src/module/colaborators/ui/views/colaborators-view.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import CollaboratorList from "../components/collaborator-list";
+import CollaboratorHeaderList from "../components/collaborators-header-list";
+
 const ColaboratorsView = () => (
-  <div>Colaborators view</div>
+  <div>
+    <CollaboratorHeaderList />
+    <CollaboratorList />
+  </div>
 );
  
 export default ColaboratorsView;
