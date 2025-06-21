a27373fbf4a70a0b2b9fd1de462a5914e49da88d
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-20 18:11:48 -0600

    add session checkiong to collaborator page

--- src/app/organizations/[organizationId]/(dashboard)/collaborators/page.tsx
+++ src/app/organizations/[organizationId]/(dashboard)/collaborators/page.tsx
@@ -1,5 +1,19 @@
-import ColaboratorsView from "@dentist/module/colaborators/ui/views/colaborators-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const ColaboratorsPage = () => <ColaboratorsView />;
+import ColaboratorsView from "@dentist/module/colaborators/ui/views/colaborators-view";
+import { auth } from "@dentist/utils/auth";
+
+const ColaboratorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
+
+  return <ColaboratorsView />;
+};
  
 export default ColaboratorsPage;
